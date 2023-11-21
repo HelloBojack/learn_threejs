59cb726dcc245c2abfc9c0d7ce367524ca7abe9c
HelloBojack <xlk970115@gmail.com>
2023-11-21 23:02:55 +0800

    feat: gsap

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import gsap from 'gsap'
 
 const scene = new THREE.Scene()
 
@@ -20,15 +21,24 @@ document.body.appendChild(renderer.domElement);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.z = 5;
 
-// renderer.render(scene, camera)
 
+// renderer.render(scene, camera)
+// console.log(gsap)
+
+// const clock = new THREE.Clock()
+
+gsap.to(cube.position, { duration: 3, y: 2, })
 
 function animate() {
+  // const elapsedTime = clock.getElapsedTime()
 
   requestAnimationFrame(animate);
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  // cube.rotation.x += elapsedTime
+  // cube.rotation.y += elapsedTime
+
+  // cube.rotation.x += 0.01
+  // cube.rotation.y += 0.01
 
   renderer.render(scene, camera);
 
