cc5c6b7fdb2cc66977e974519600dc9f8a22f83a
HelloBojack <xlk970115@gmail.com>
2023-11-22 14:00:31 +0800

    feat: OrbitControls

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import gsap from 'gsap'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 const scene = new THREE.Scene()
 
@@ -19,16 +20,29 @@ const renderer = new THREE.WebGLRenderer();
 document.body.appendChild(renderer.domElement);
 
 renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.z = 5;
+// camera.position.z = 5;
 
+// const cursor = {
+//   x: 0,
+//   y: 0
+// }
+
+// window.addEventListener('mousemove', (event) => {
+//   cursor.x = event.clientX / window.innerWidth - 0.5
+//   cursor.y = -(event.clientY / window.innerHeight - 0.5)
+// })
 
 // renderer.render(scene, camera)
 // console.log(gsap)
 
 // const clock = new THREE.Clock()
 
-gsap.to(cube.position, { duration: 3, y: 2, })
+// gsap.to(cube.position, { duration: 3, y: 2, })
 
+const controls = new OrbitControls(camera, renderer.domElement);
+
+camera.position.set(0, 2, 0);
+controls.update();
 function animate() {
   // const elapsedTime = clock.getElapsedTime()
 
@@ -37,8 +51,15 @@ function animate() {
   // cube.rotation.x += elapsedTime
   // cube.rotation.y += elapsedTime
 
-  // cube.rotation.x += 0.01
-  // cube.rotation.y += 0.01
+  // camera.position.x = cursor.x * 10
+  // camera.position.y = cursor.y * 10
+
+  // camera.position.x = Math.sin(cursor.x * 2 * Math.PI) * 2
+  // camera.position.z = Math.cos(cursor.x * 2 * Math.PI) * 2
+  // camera.position.y = cursor.y * 5
+  // camera.lookAt(cube.position)
+
+  controls.update();
 
   renderer.render(scene, camera);
 
